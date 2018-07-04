97efd832618769d4f623d699cfea2e52c314d096
Matt Lintlop <mattlintlop@matts-macbook-pro.local>
2018-07-04 13:22:42 -0700

    Added a sctrolling stackvoew of images!

--- StackerUp/ViewController.swift
+++ StackerUp/ViewController.swift
@@ -10,18 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var pinkView: UIView!
-    @IBOutlet weak var yellowView: UIView!
-    @IBOutlet weak var blueView: UIView!
+
+
+    @IBOutlet weak var stackviewPlaceholderView: UIView!
     var scrollView: UIScrollView!
     var stackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+
         scrollView = UIScrollView()
         scrollView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(scrollView)
+        stackviewPlaceholderView.addSubview(scrollView)
         
         view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[scrollView]|", options: .alignAllCenterX, metrics: nil, views: ["scrollView": scrollView]))
         view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[scrollView]|", options: .alignAllCenterX, metrics: nil, views: ["scrollView": scrollView]))
@@ -29,16 +30,16 @@ class ViewController: UIViewController {
         
         stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .vertical
+        stackView.axis = .horizontal
+        stackView.spacing = 10
         scrollView.addSubview(stackView)
         
         scrollView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[stackView]|", options: NSLayoutConstraint.FormatOptions.alignAllCenterX, metrics: nil, views: ["stackView": stackView]))
         scrollView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[stackView]", options: NSLayoutConstraint.FormatOptions.alignAllCenterX, metrics: nil, views: ["stackView": stackView]))
         
-        for _ in 1 ..< 100 {
-            let vw = UIButton(type: UIButton.ButtonType.system)
-            vw.setTitle("Button", for: .normal)
-            stackView.addArrangedSubview(vw)
+        for _ in 1 ..< 50 {
+            let imageView = UIImageView(image: UIImage(named: "testPhoto"))
+            stackView.addArrangedSubview(imageView)
         }
     }
     
