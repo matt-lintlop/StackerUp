503138c214845b9d0c6001ef41c7e54f51db318d
Matt Lintlop <mattlintlop@matts-macbook-pro.local>
2018-07-04 17:03:25 -0700

    Implemented adding & deleting buttons to the stack view.

--- StackerUp/ViewController.swift
+++ StackerUp/ViewController.swift
@@ -24,9 +24,9 @@ class ViewController: UIViewController {
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         stackviewPlaceholderView.addSubview(scrollView)
         
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[scrollView]|", options: .alignAllCenterX, metrics: nil, views: ["scrollView": scrollView]))
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[scrollView]|", options: .alignAllCenterX, metrics: nil, views: ["scrollView": scrollView]))
-        
+    view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[scrollView]|", options: .alignAllCenterX, metrics: nil, views: ["scrollView": scrollView]))
+    view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[scrollView]|", options: .alignAllCenterX, metrics: nil, views: ["scrollView": scrollView]))
+    
         
         stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -34,28 +34,33 @@ class ViewController: UIViewController {
         stackView.spacing = 10
         scrollView.addSubview(stackView)
         
-        scrollView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[stackView]|", options: NSLayoutConstraint.FormatOptions.alignAllCenterX, metrics: nil, views: ["stackView": stackView]))
-        scrollView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[stackView]", options: NSLayoutConstraint.FormatOptions.alignAllCenterX, metrics: nil, views: ["stackView": stackView]))
-        
-        for i in 1...51 {
- //           let imageView = UIImageView(image: UIImage(named: "testPhoto"))
-
-            let view = UIButton(type: .roundedRect)
- //           let view = UIButton(frame: CGRect(x: 0, y: 0, width: 74  , height: 74))
-            view.widthAnchor.constraint(equalToConstant: 74).isActive = true
-            view.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1.0).isActive = true
-            view.setTitle(String(i), for: .normal)
-            view.setTitleColor( UIColor.red, for: .normal)
-            view.backgroundColor = UIColor.lightGray
-
-//            view.widthAnchor.constraint(equalToConstant: 76).isActive = true
-//            view.heightAnchor.constraint(equalToConstant: 76).isActive = true
-//            let label = UILabel(frame: CGRect.zero)
-//            label.text = String(i)
-//            label.textColor = UIColor.red
-   //         view.addSubview(label)
-            stackView.addArrangedSubview(view)
+    scrollView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[stackView]|", options: NSLayoutConstraint.FormatOptions.alignAllCenterX, metrics: nil, views: ["stackView": stackView]))
+    scrollView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[stackView]", options: NSLayoutConstraint.FormatOptions.alignAllCenterX, metrics: nil, views: ["stackView": stackView]))
+    
+        for i in 1...20 {
+            addView()
         }
+    }
+    
+    fileprivate func addView() {
+        let view = UIButton(type: .roundedRect)
+        //           let view = UIButton(frame: CGRect(x: 0, y: 0, width: 74  , height: 74))
+        view.widthAnchor.constraint(equalToConstant: 74).isActive = true
+        view.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1.0).isActive = true
+        view.setTitle(String(stackView.arrangedSubviews.count+1), for: .normal)
+        view.setTitleColor( UIColor.red, for: .normal)
+        view.backgroundColor = UIColor.lightGray
+        
+        view.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
+        stackView.addArrangedSubview(view)
+    }
+    
+    @IBAction func addViewPressed(_ sender: Any) {
+        addView()
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.isHidden = true
     }
     
     override func viewDidLayoutSubviews() {
